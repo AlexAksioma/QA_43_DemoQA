df3fdca537aa6123dc6e6aa8794f20530b30379b
AlexAksioma <alexmedqwerty@gmail.com>
2024-08-04 21:21:10 +0300

    added exceptions, collections

--- src/main/java/experiments/MyExceptions.java
+++ src/main/java/experiments/MyExceptions.java
@@ -1,2 +1,32 @@
-package experiments;public class MyExceptions {
+package experiments;
+
+public class MyExceptions {
+    public static void main(String[] args) {
+        method1();
+        uncheckedException(10, 0);
+    }
+
+    public static void method1() {
+        try {
+            pauseThread();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void pauseThread() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    public static void uncheckedException(int a, int b) {
+        int result = 0;
+        try {
+            result = a / b;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            result = 1;
+            System.out.println(result);
+        }
+    }
+
 }
